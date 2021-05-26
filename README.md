0e4ba8fb60958806a04e264540fd6cc78936b5f4
迷渡 <justjavac@gmail.com>
2021-05-26 14:17:26 +0800

    release v1.0.0
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# deno_deploy_readme
+# Deno Deploy Readme
+
+> Handle GitHub Readme for Deno Deploy.
+
+## Deploy the code
+
+1. Click on the button below and you'll be navigated to Deno Deploy.
+
+    [![](./assets/deno-deploy-button.svg)](https://dash.deno.com/new?url=https://raw.githubusercontent.com/justjavac/deno_deploy_readme/mian/mod.ts&env=GITHUB_REPO,GITHUB_BRANCH)
+
+1. Input `GITHUB_REPO` and `GITHUB_BRANCH` env variable field. If `GITHUB_BRANCH` is not set, the default branch is `main`.
+
+1. Click on `Create` to create the project, then on `Deploy` to deploy the script.
+
+1. Grab the URL that's displayed under `Domains` in Production Deployment card.
+
+## Local Dev
+
+Run on local:
+
+```bash
+$ LOCAL=true deployctl run --watch --no-check mod.ts
+```
+
+## License
+
+[Deno Deploy Readme](https://github.com/justjavac/deno_deploy_readme) is released under the MIT License. See the bundled [LICENSE](./LICENSE) file for details.
---
+++ mod.ts
@@ -0,0 +1,69 @@
+import { html } from "https://deno.land/x/html/mod.ts";
+import unified from "https://esm.sh/unified";
+import markdown from "https://esm.sh/remark-parse";
+import remark2rehype from "https://esm.sh/remark-rehype";
+import externalLinks from "https://esm.sh/remark-external-links";
+import stringify from "https://esm.sh/rehype-stringify";
+
+function getUrl(fileName: string) {
+  if (Deno.env.get("LOCAL")) {
+    return new URL(fileName, import.meta.url)
+  }
+
+  const repo = Deno.env.get("GITHUB_REPO") ?? "justjavac/deno_deploy_readme";
+  const branch = Deno.env.get("GITHUB_BRANCH") ?? "main";
+
+  return `https://raw.githubusercontent.com/${repo}/${branch}/${fileName}`;
+}
+
+async function handleRequest(request: Request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.startsWith("/assets")) {
+    const favicon = new URL(pathname.substr(1), import.meta.url);
+    return fetch(favicon);
+  }
+
+  const response = await fetch(getUrl("README.md"));
+  const content = await response.text();
+
+  const output = await unified()
+    .use(markdown)
+    .use(externalLinks)
+    .use(remark2rehype)
+    .use(stringify)
+    .process(content);
+
+  return new Response(
+    html`<!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+          <title>Deno Deploy Readme</title>
+          <link rel="stylesheet" href="/assets/style.css" />
+        </head>
+        <body>
+          <div class="container-lg px-3 my-5 markdown-body">
+            ${output.toString()}
+          </div>
+          <script src="https://cdn.bootcdn.net/ajax/libs/anchor-js/4.2.2/anchor.min.js"></script>
+          <script>
+            anchors.add();
+          </script>
+        </body>
+      </html>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    }
+  );
+}
+
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request));
+});
